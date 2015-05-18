44e13a6594a18edb4083a8b4052102e6f1fdb3e3
Kelsey Hightower <kelsey.hightower@gmail.com>
2015-05-18 07:44:41 -0700

    terraform: add docs on increasing kubernetes worker size.

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Provision a Kubernetes cluster with [Packer](https://packer.io) and [Terraform](
 
 ## Packer Images
 
+Immutable infrastructure is the future. Instead of using cloud-init to provision machines at boot we'll create custom images using Packer for the Kubernetes server and worker machines.
+
 ```
 cd packer
 ```
@@ -28,6 +30,8 @@ packer build kubestack-worker.json
 
 ## Terraform
 
+Terraform will be used to declare and provision a Kubernetes cluster.
+
 ```
 cd terraform
 ```
@@ -37,5 +41,21 @@ terraform plan
 ```
 
 ```
+terraform apply
+```
+
+### Increase the number of worker nodes
+
+Edit terraform/kubestack.tf
+
+```
+resource "google_compute_instance" "kubelet" {
+    count = 5
+```
+
+Increase the count to your desired instance count.
+
+```
+terraform plan
 terraform apply
 ```
--- terraform/kubestack.tf
+++ terraform/kubestack.tf
@@ -5,25 +5,29 @@ provider "google" {
 }
 
 resource "google_compute_instance" "etcd" {
+    count = 3
+
     name = "etcd${count.index}"
     machine_type = "n1-standard-1"
+    can_ip_forward = true
     zone = "us-central1-a"
     tags = ["etcd"]
+
     disk {
         image = "kubestack-0-0-1-v20150517"
         size = 200
     }
-    can_ip_forward = true
+
     network_interface {
         network = "default"
         access_config {
             // Ephemeral IP
         }
     }
+
     metadata {
         "sshKeys" = "${file("sshkey")}"
     }
-    count = 3
 
     provisioner "file" {
         source = "units/etcd${count.index}.service"
@@ -50,47 +54,55 @@ resource "google_compute_instance" "etcd" {
 resource "google_compute_instance" "kube-apiserver" {
     name = "kube-apiserver"
     machine_type = "n1-standard-1"
+    can_ip_forward = true
     zone = "us-central1-a"
     tags = ["kubernetes", "kube-apiserver"]
+
     disk {
         image = "kubestack-server-0-0-1-v20150517"
         size = 200
     }
-    can_ip_forward = true
+
     network_interface {
         network = "default"
         access_config {
             // Ephemeral IP
         }
     }
+
     metadata {
         "sshKeys" = "${file("sshkey")}"
     }
+
     depends_on = [
         "google_compute_instance.etcd",
     ]
 }
 
 resource "google_compute_instance" "kubelet" {
+    count = 5
+
     name = "kubelet${count.index}"
+    can_ip_forward = true
     machine_type = "n1-standard-1"
     zone = "us-central1-a"
     tags = ["kubelet", "kubernetes"]
+
     disk {
         image = "kubestack-worker-0-0-1-v20150517"
         size = 200
     }
-    can_ip_forward = true
+
     network_interface {
         network = "default"
         access_config {
             // Ephemeral IP
         }
     }
+
     metadata {
         "sshKeys" = "${file("sshkey")}"
     }
-    count = 5
 
     depends_on = [
         "google_compute_instance.kube-apiserver",
